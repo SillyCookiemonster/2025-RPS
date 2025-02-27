42c93f5fafe13481de2fa86946d67fce864c991f
SillyCookiemonster <meyere3109@masseyhigh.school.nz>
2025-02-28 11:44:01 +1300

    String Checker (yes/no) & (r/p/s)

---
+++ C_02_String_checker_v3.py
@@ -0,0 +1,36 @@
+# Check that users have entered a valid
+# option based on a list
+def string_checker(question, valid_ans=["yes", "no"]):
+
+    error = f"Please enter a value from the following list: {valid_ans}"
+
+    while True:
+
+        # Get user response and make sure it is lowercase
+        user_response = input(question).lower()
+
+        for item in valid_ans:
+            # check if the user response is a word in the list
+            if item == user_response:
+                return item
+            # check if the user response
+            # the first letter of an item in the list
+            elif user_response == item[0]:
+                return item
+
+        # print error if user does not enter something valid
+        print(error)
+        print()
+
+
+# Main code
+
+
+rps_list = ["rock", "paper", "scissors", "xxx"]
+
+want_instructions = string_checker("Do you want to see instructions? ")
+
+
+print("You chose: ", want_instructions)
+rps_ans = string_checker("Rock, Paper, Scissors: ", rps_list)
+print("You chose: ", rps_ans)
